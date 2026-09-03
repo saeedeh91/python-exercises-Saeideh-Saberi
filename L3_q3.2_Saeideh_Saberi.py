5f0d66c3efe3214b16f86ba332c888bec41bb9c2
saeedeh91 <saeidehsaberii911@gmail.com>
2026-09-03 17:46:34 +0330

    the q3.2 is completed

---
+++ L3_q3.2_Saeideh_Saberi.py
@@ -0,0 +1,15 @@
+'''
+session-03 
+
+q3.2
+----------Even and Odd-------------
+
+'''
+
+
+a = int(input("enter the number:"))
+
+if a % 2 == 0:
+    print('the number is Even!!!!!!')
+else: 
+    print('the number is Odd!!!')

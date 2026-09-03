7c3812216125c791507a5d7710bebcd4304eab01
saeedeh91 <saeidehsaberii911@gmail.com>
2026-09-03 17:46:02 +0330

    the q1 is completed

---
+++ L3_q3.1_Saeideh_Saberi.py
@@ -0,0 +1,17 @@
+'''
+session-03 
+
+q3.1
+
+'''
+
+a = int(input("enter the number:"))
+
+print(a)
+
+if a == 0:
+    print("Zero")
+elif a>= 1:
+    print('positive')
+else:
+    print("Negative")
